46bc28b104a8c22726ffd8b01610996bf5ba54d8
Vladimir Grigorovich <37683474+grigvlwork@users.noreply.github.com>
2022-01-18 15:00:43 +0700

    добавил операции с матрицами

---
+++ matrix_oper.py
@@ -0,0 +1,29 @@
+def rotate_matrix_right(a):
+    rows = len(a)
+    cols = len(a[0])
+    b = [[0] * rows for i in range(cols)]
+    for i in range(rows):
+        for j in range(cols):
+            b[j][rows - i - 1] = a[i][j]
+    return b
+
+def rotate_matrix_left(a):
+    rows = len(a)
+    cols = len(a[0])
+    b = [[0] * rows for i in range(cols)]
+    for i in range(rows):
+        for j in range(cols):
+            b[cols - j - 1][i] = a[i][j]
+    return b
+
+def mirror_matrix(a):
+    rows = len(a)
+    cols = len(a[0])
+    b = [[0] * cols for i in range(rows)]
+    for i in range(rows):
+        for j in range(cols):
+            b[rows - i - 1][j] = a[i][j]
+    return b
+
+s = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(mirror_matrix(s))
